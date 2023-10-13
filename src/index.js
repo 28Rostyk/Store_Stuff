a5beb27b25685270299204e15c657009ffb988a7
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-10-13 20:36:49 +0300

    add redux products and categories

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./styles/index.css";
 
 import App from "./components/App/App";
+import { store } from "./features/store";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
