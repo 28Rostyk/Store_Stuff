809a5796688bdd466136872c72ba297fcd25de71
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2024-01-05 00:13:30 +0200

    created loader

--- src/components/Products/FavouriteProducts.jsx
+++ src/components/Products/FavouriteProducts.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Products from "./Products";
 import { useSelector } from "react-redux";
+import Loader from "../../shared/Loader/Loader";
 
 const FavouriteProducts = () => {
-  const { favourite } = useSelector(({ user }) => user);
+  const { favourite, loading } = useSelector(({ user }) => user);
   return (
     <>
       <Products products={favourite} amount={5} title="Улюблене" />
+      {loading && <Loader />}
     </>
   );
 };
---
+++ src/shared/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Loader.module.css";
+
+const Loader = () => {
+  return <div className={styles.preloader}></div>;
+};
+
+export default Loader;
